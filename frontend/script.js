edc6790a34b42a0fb4f7adb535e2312e9e61a187
BigdreamsEng <joshuabigdreams415@gmail.com>
2025-01-17 22:33:37 +0100

    modified

--- frontend/script.js
+++ frontend/script.js
@@ -20,3 +20,14 @@ function closeSidebar() {
   mobileSidebar.classList.remove('active');
   overlay.classList.remove('active');
 }
+
+
+//recaptcha
+
+
+function onClick(e) {
+  e.preventDefault();
+  grecaptcha.enterprise.ready(async () => {
+    const token = await grecaptcha.enterprise.execute('6LelZLIqAAAAAHu2RwA7HNw6NTzuumUs4g5K8uQ3', {action: 'LOGIN'});
+  });
+}
